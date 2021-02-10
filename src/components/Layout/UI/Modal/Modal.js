9238324a26409808d73421a374f1580c354007f8
Mayank <hellomayankashok@gmail.com>
2021-02-10 10:45:20 -0800

    Lecture 139: completed - Implementing Backdrop Component.

--- src/components/Layout/UI/Modal/Modal.js
+++ src/components/Layout/UI/Modal/Modal.js
@@ -6,7 +6,7 @@ import Backdrop from '../Backdrop/Backdrop';
 const modal = (props) => (
 		<Aux>
 			<Backdrop 
-				show={props.show} modalClosed={props.modalClosed}/>
+				show={props.show} clicked={props.modalClosed}/>
 			<div 
 				className={classes.Modal}
 				style={{
