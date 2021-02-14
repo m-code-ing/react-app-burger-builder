a208358a0b928b815c4695cd23fd468d8a1d48b4
Mayank <hellomayankashok@gmail.com>
2021-02-13 23:45:03 -0800

    Lecture 229: completed.
    - Implementing Navigations Links completed.
    
    see you in a bit..

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import classes from './NavigationItem.module.css'
 
@@ -6,8 +7,10 @@ import classes from './NavigationItem.module.css'
 const navigationItem = (props) => {
     return (
         <li className={classes.NavigationItem}>
-            <a href={props.link}
-                className={props.active ? classes.active : null}>{props.children}</a>
+            <NavLink
+                to={props.link}
+                exact={props.exact}
+                activeClassName={classes.active}>{props.children}</NavLink>
         </li>
     )
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,34 +86,12 @@ class BurgerBuilder extends Component {
 
 	purcchaseContinueHandler = () => {
 		// alert('You continue!');
-		// this.setState({loading: true});
-		// const order = {
-		// 	ingredients: this.state.ingredients,
-		// 	price: this.state.totalPrice.toFixed(2),
-		// 	customer: {
-		// 		name: 'Mayank',
-		// 		address: {
-		// 			street: 'Teststreet 1',
-		// 			zipCode: '123412',
-		// 			country: 'Canada'
-		// 		},
-		// 		email: 'test@test.com'
-		// 	},
-		// 	deliveryMethod: 'fastest'
-		// }
-
-		// axios.post('/orders.json', order)
-		// 	.then(response => {
-		// 		this.setState({loading: false, purchasing: false});
-		// 	})
-		// 	.catch(error => {
-		// 		this.setState({loading: false, purchasing: false});
-		// 	});
-
+		
 		const queryParams = [];
 		for(let i in this.state.ingredients){
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
+		queryParams.push('price=' + this.state.totalPrice);
 		const queryString = queryParams.join('&');
 
 		this.props.history.push({
