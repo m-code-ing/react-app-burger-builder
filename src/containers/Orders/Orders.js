623141d94b0f1c7521aef9756a948ecca96013c5
Mayank <hellomayankashok@gmail.com>
2021-02-14 08:39:08 -0800

    Lecture 230: completed with error...
    - withErrorHandler is working as a wrapping higher order Component..
    but not showing errors.. as the axios.interceptors are not being
    triggered.. have researched solution.. but not found anything significant..
    Need to research further..
    
    - for now handling errors locally in the Orders.js file..
    I assume the withErrorHandler is also not working else where..
    such as ContactOrder.js.. this was added by me.. and i think was not in
    the lectures..
    
    Completed with error.. find this if required.. to correct later..
    
    see you in a bit..

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,33 +3,41 @@ import React, { Component } from 'react'
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Modal from '../../components/UI/Modal/Modal';
 
 class Orders extends Component {
     state = {
         orders: [],
-        loading: true
+        loading: true,
+        error: null
     }
-    componentDidMount(){
-        axios.get('/orders')
-        .then((res) => {
-            const fetchedOrders = [];
-            for (let key in res.data) {
-                fetchedOrders.push({
-                    ...res.data[key],
-                    id: key
-                });
-            }
-            this.setState({loading: false, orders: fetchedOrders});
-        })
-        .catch((err) => {
-            this.setState({loading: false});
-        });
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then((res) => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch((err) => {
+                this.setState({ loading: false, error: err});
+            });
     }
     render() {
         return (
             <div>
-                <Order></Order>                
-                <Order></Order>                
+                <Modal
+                    show={this.state.error}
+                    // show
+                    modalClosed={this.errorConfirmedHandler}>
+                    {this.state.error ? this.state.error.message : null}
+                </Modal>
+                <Order></Order>
+                <Order></Order>
             </div>
         )
     }
